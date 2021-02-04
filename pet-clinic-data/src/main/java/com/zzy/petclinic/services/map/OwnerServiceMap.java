fa248fa0171ff135b1e002d4d30b0622736d80d1
zhaoyi3264 <zhaoyi3264@gmail.com>
2021-02-04 11:13:03 -0600

    implement map based services, closes #6

---
+++ pet-clinic-data/src/main/java/com/zzy/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package com.zzy.petclinic.services.map;
+
+import com.zzy.petclinic.model.Owner;
+import com.zzy.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+}
