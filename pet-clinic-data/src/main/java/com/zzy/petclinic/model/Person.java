f0e6bf88023cd8422eaf759ca38c68f33db489a1
zhaoyi3264 <zhaoyi3264@gmail.com>
2021-02-03 19:44:45 -0600

    adding base entity. closes #10.

--- pet-clinic-data/src/main/java/com/zzy/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/zzy/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.zzy.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/zzy/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/zzy/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.zzy.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
