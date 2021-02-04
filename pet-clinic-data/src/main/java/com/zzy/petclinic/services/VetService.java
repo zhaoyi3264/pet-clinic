1476b6a90aea113b60e6a33b4e28629379d495a8
zhaoyi3264 <zhaoyi3264@gmail.com>
2021-02-03 19:39:25 -0600

    implement interfaces for services. closes #5.

---
+++ pet-clinic-data/src/main/java/com/zzy/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.zzy.petclinic.services;
+
+import com.zzy.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
