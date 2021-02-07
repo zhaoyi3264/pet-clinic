2f8bcab7e305a28aaa2df6c480aee44dc8631d86
zhaoyi3264 <zhaoyi3264@gmail.com>
2021-02-06 20:30:20 -0600

    add bootstrap data. closes #14

--- pet-clinic-data/src/main/java/com/zzy/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/zzy/petclinic/services/map/OwnerServiceMap.java
@@ -2,10 +2,11 @@ package com.zzy.petclinic.services.map;
 
 import com.zzy.petclinic.model.Owner;
 import com.zzy.petclinic.services.CrudService;
+import com.zzy.petclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -29,5 +30,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public Owner save(Owner object) {
         return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
--- pet-clinic-data/src/main/java/com/zzy/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/zzy/petclinic/services/map/VetServiceMap.java
@@ -2,10 +2,11 @@ package com.zzy.petclinic.services.map;
 
 import com.zzy.petclinic.model.Vet;
 import com.zzy.petclinic.services.CrudService;
+import com.zzy.petclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
