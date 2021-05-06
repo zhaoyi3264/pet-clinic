80600a65389c5de10033612c87a22a803c0a00ad
zhaoyi3264 <zhaoyi3264@gmail.com>
2021-05-06 14:55:05 -0500

    added PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/com/zzy/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,34 @@
+package com.zzy.petclinic.services.map;
+
+import com.zzy.petclinic.model.PetType;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService{
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/zzy/petclinic/services/map/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.zzy.petclinic.services.map;
+
+import com.zzy.petclinic.model.PetType;
+import com.zzy.petclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
