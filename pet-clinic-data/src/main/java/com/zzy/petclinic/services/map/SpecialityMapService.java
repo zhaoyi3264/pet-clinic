77b856ede1c6c7f6f5e2d4cec24ea529723e87d0
zhaoyi3264 <zhaoyi3264@gmail.com>
2021-05-06 17:16:47 -0500

    added speciality map impl. close #31

---
+++ pet-clinic-data/src/main/java/com/zzy/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,34 @@
+package com.zzy.petclinic.services.map;
+
+import com.zzy.petclinic.model.Speciality;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/zzy/petclinic/services/map/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.zzy.petclinic.services.map;
+
+import com.zzy.petclinic.model.Speciality;
+import com.zzy.petclinic.services.CrudService;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
