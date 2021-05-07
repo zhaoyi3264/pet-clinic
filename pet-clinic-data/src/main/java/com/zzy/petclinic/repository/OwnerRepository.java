b0f7c58dc80b26c0a0795a3a3baa7c6ccb51c671
zhaoyi3264 <zhaoyi3264@gmail.com>
2021-05-07 17:15:55 -0500

    added spring data jpa service for owners. closes #40

--- pet-clinic-data/src/main/java/com/zzy/petclinic/repository/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/zzy/petclinic/repository/OwnerRepository.java
@@ -4,4 +4,5 @@ import com.zzy.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
