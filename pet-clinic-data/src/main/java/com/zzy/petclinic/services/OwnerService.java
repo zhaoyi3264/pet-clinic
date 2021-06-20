a7f0fb94ad992d19bf8d051e867cf821e4774b1c
zhaoyi3264 <zhaoyi3264@gmail.com>
2021-06-20 13:48:51 -0500

    create Speciality JPA services. closes #44 #42

--- pet-clinic-data/src/main/java/com/zzy/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/zzy/petclinic/services/OwnerService.java
@@ -2,7 +2,7 @@ package com.zzy.petclinic.services;
 
 import com.zzy.petclinic.model.Owner;
 
-public interface OwnerService extends CrudService<Owner, Long>{
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
---
+++ pet-clinic-data/src/main/java/com/zzy/petclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.zzy.petclinic.services;
+
+import com.zzy.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
