4cda0cf253c72601139a24594b10a231ed404b35
zhaoyi3264 <zhaoyi3264@gmail.com>
2021-06-20 13:40:36 -0500

    create PetTYpe JPA services. closes #41

---
+++ pet-clinic-data/src/main/java/com/zzy/petclinic/services/jpa/PetTypeJpaService.java
@@ -0,0 +1,48 @@
+package com.zzy.petclinic.services.jpa;
+
+import com.zzy.petclinic.model.PetType;
+import com.zzy.petclinic.repository.PetTypeRepository;
+import com.zzy.petclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class PetTypeJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
